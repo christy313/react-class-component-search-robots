cd9fefb26bac2cafe1d4c0d3d3bfdca56377a2b3
christy313 <christycreativity@gmail.com>
2022-08-25 16:57:41 -0400

    Create Hello component

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Hello.css";
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hiiiiiiiiii</h1>
+        <p>yoyoyoyoyoyo</p>
+      </div>
+    );
+  }
+}
