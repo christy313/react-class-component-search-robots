bf81353d4ceaf3cc77b1541a195af9380cd6b2f1
christy313 <christycreativity@gmail.com>
2022-08-26 13:30:06 -0400

    Add SearchBox component for search robots

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import CardList from "./CardList";
+import App from "./App";
 import { robots } from "./robots";
 import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<CardList robots={robots} />);
+root.render(<App robots={robots} />);
