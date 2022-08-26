d14b82f402c866a458a22bb87e81f2fe7f29377d
christy313 <christycreativity@gmail.com>
2022-08-25 20:15:05 -0400

    Delete unused files like App.css and create Card component

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Card = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>abc@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
