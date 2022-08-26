5ccb43f1fd06da7c49c6a934828ea4f75632bbb0
christy313 <christycreativity@gmail.com>
2022-08-26 09:52:39 -0400

    Add Scroll component to keep search bar visible while scrolling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import Scroll from "./Scroll";
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (e) => {
@@ -22,12 +28,18 @@ export default class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1>Search Robots</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filterRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1>Search Robots</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filterRobots} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
