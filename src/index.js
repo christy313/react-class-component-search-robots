7fb50e5af6d4c2d294b638d4a641597027cff56c
christy313 <christycreativity@gmail.com>
2022-08-26 13:16:22 -0400

    Create CardList component to wrap all cards inside

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Card from "./Card";
+import CardList from "./CardList";
 import { robots } from "./robots";
 import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
-  </div>
-);
+root.render(<CardList robots={robots} />);
