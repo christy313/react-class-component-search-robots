81c2258bb1fa22b440fcb06459c799b8fcc85249
christy313 <christycreativity@gmail.com>
2022-08-27 16:37:01 -0400

    Pass props from index.js to Card component

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ id, name, email }) => {
   return (
-    <div>
-      <img src="" alt="robots" />
+    <div className="bg-light-green dib br3 pa2 ma2 grow tc bw2 shadow-5">
+      <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
       <div>
-        <h2>name</h2>
-        <p>email@email.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
