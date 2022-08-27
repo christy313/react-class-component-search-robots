1fb7e063ced61485a422d244a951d65852072326
christy313 <christycreativity@gmail.com>
2022-08-27 14:25:05 -0400

    Do the first component Hello and install tachyons

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Hello.css";
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello</h1>
+        <p>welcome to react</p>
+      </div>
+    );
+  }
+}
