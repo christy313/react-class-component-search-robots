2302598e07ae687fc71d1be20d5eab2ea7207188
christy313 <christycreativity@gmail.com>
2022-08-26 22:16:19 -0400

    Add componentDidMount and fetch data from jsonplaceholder to get users

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>Search Robots</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
 
-export default App;
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (e) => {
+    this.setState({ searchfield: e.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>Search Robots</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
