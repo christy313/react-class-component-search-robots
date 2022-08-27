6643a720da643832364180ba24a2133a61e81843
christy313 <christycreativity@gmail.com>
2022-08-27 14:57:23 -0400

    Build Card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img src="" alt="robots" />
+      <div>
+        <h2>name</h2>
+        <p>email@email.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
 import "tachyons";
+import Card from "./Card";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Card />
   </React.StrictMode>
 );
