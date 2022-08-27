ff7e139e6cac8d5c6e9b707aef45350ea8de3bd8
christy313 <christycreativity@gmail.com>
2022-08-26 22:43:54 -0400

    Add ErrorBoundary component to show errors in the view

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 export default class App extends Component {
   constructor() {
@@ -36,7 +37,9 @@ export default class App extends Component {
           <h1>Search Robots</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
           </Scroll>
         </div>
       );
