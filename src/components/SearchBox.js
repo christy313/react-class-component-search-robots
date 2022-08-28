7455b4ba2158f6f95c2c3d593a0908e8fc955d38
christy313 <christycreativity@gmail.com>
2022-08-28 16:05:09 -0400

    Fix search bug & modify SearchChange to searchChange

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const SearchBox = ({ SearchChange }) => {
+const SearchBox = ({ searchChange }) => {
   return (
     <div className="pa2">
       <input
         type="search"
         placeholder="search robots"
         className="pa3 ba b--green bg-lightest-blue"
-        onChange={SearchChange}
+        onChange={searchChange}
       />
     </div>
   );
