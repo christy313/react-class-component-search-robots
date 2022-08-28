7dc776cfa9d2a26efae3d3969bea5bf0c7d44261
christy313 <christycreativity@gmail.com>
2022-08-28 16:00:48 -0400

    Chore: adjust import order in App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import CardList from "../components/CardList";
-import "tachyons";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
+import "tachyons";
 
 export default class App extends Component {
   constructor() {
@@ -26,7 +26,6 @@ export default class App extends Component {
 
   render() {
     const { robots, searchfield } = this.state;
-
     const filteredRobots = robots.filter((robot) =>
       robot.name.toLowerCase().includes(searchfield.toLowerCase())
     );
