4ea46fcee31993bb4113854398f47f541cdb2e11
christy313 <christycreativity@gmail.com>
2022-08-28 14:29:22 -0400

    Fetch users API from JSONplaceholder& setState

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from "react";
 import CardList from "../components/CardList";
 import "tachyons";
-import { robots } from "../robots";
 import SearchBox from "../components/SearchBox";
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
   }
 
-  onSearchChange(e) {
-    console.log(e.target.value);
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
+  onSearchChange = (e) => {
+    this.setState({ searchfield: e.target.value });
+  };
+
   render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
     return (
       <div className="tc">
         <h1>Search Robots</h1>
         <SearchBox SearchChange={this.onSearchChange} />
-        <CardList robots={robots} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
