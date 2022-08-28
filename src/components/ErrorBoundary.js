0dac665d68c98495ac5558fb2f7547313c37bb76
christy313 <christycreativity@gmail.com>
2022-08-28 12:51:49 -0400

    Add ErrorBoundary component to show the error in client side

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class ErrorBoundary extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hasError: false,
+    };
+  }
+  componentDidCatch(error, info) {
+    this.setState({ hadError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong here!</h1>;
+    }
+    return this.props.children;
+  }
+}
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 import "tachyons";
 
 export default class App extends Component {
@@ -37,7 +38,9 @@ export default class App extends Component {
           <h1>Search Robots</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
           </Scroll>
         </div>
       );
