59e4133e28b9e78584b2cbb4c43e6f14b081ee64
christy313 <christycreativity@gmail.com>
2022-08-28 13:25:53 -0400

    Add SearchBox component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import "tachyons";
 import { robots } from "../robots";
+import SearchBox from "../components/SearchBox";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="tc">
+        <h1>Search Robots</h1>
+        <SearchBox />
         <CardList robots={robots} />
       </div>
     );
